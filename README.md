9dcdf3825a103350c4db8eaf0f1caa81897c9227
舒婧 <s223568907@126.com>
2022-08-27 20:20:46 +0800

    展示图片

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# matlab
+# 一些可能会用到的matlab函数
 心理学matlab可供参考的函数和代码文件
+
+## showPic(winPtr, winRect, picName, time)
+
+用于展示图片。
+
+- winPtr/winRect：通过命令`[winPtr, winRect] = Screen('OpenWindow', screenNumber, background)`获取的值。
+- picName：需要展示的图片的路径，如`1.png`，类型为字符串。
+- time：希望图片展示的时长（单位为秒），可以不传此参数表示呈现图片直至按下空格，类型为数字。
---
+++ showPic.m
@@ -0,0 +1,30 @@
+function showPic(winPtr, winRect, picName, time)
+
+    if nargin < 4
+        time = -1;
+    end
+
+    space = KbName('space'); // 可以修改成其他按键
+
+    intro = imread(picName);
+    size = size(intro);
+    introRect = CenterRect([0 0 size1(2) size1(1)], winRect);
+    introTexture = Screen('MakeTexture', winPtr, intro);
+    Screen('DrawTexture', winPtr, introTexture, [], introRect);
+    Screen(winPtr, 'Flip');
+
+    if time < 0
+        KbWait([], 2, []);
+        [~, ~, keycode] = KbCheck;
+
+        while ~keycode(space)
+            KbWait;
+            [~, ~, keycode] = KbCheck;
+            WaitSecs(0.001);
+        end
+
+    else
+        WaitSecs(time);
+    end
+
+end
